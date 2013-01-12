8b22dfe77f27813322310e77d7a2e1c4e5ff328b
Andreas von Studnitz <avs@integer-net.de>
2013-01-12 15:59:01 +0100

    [+Task] Added pretty printing to JSON export

--- src/app/code/community/FireGento/AlternativeContentStorage/Model/Storage/File.php
+++ src/app/code/community/FireGento/AlternativeContentStorage/Model/Storage/File.php
@@ -60,7 +60,7 @@ class FireGento_AlternativeContentStorage_Model_Storage_File extends FireGento_A
      */
     public function storeData($data, $entityType) {
 
-        $fileContent = Zend_Json::encode($data);
+        $fileContent = $this->prettyPrint(Zend_Json::encode($data));
 
         $fileName = $this->_getStorageDirectory() . $entityType . '.json';
 
@@ -69,5 +69,59 @@ class FireGento_AlternativeContentStorage_Model_Storage_File extends FireGento_A
                 Mage::helper('acs')->__('File "%s" could not be written.')
             );
         };
+    }
+
+    protected function prettyPrint( $json )
+    {
+        $result = '';
+        $level = 0;
+        $prev_char = '';
+        $in_quotes = false;
+        $ends_line_level = NULL;
+        $json_length = strlen( $json );
+
+        for( $i = 0; $i < $json_length; $i++ ) {
+            $char = $json[$i];
+            $new_line_level = NULL;
+            $post = "";
+            if( $ends_line_level !== NULL ) {
+                $new_line_level = $ends_line_level;
+                $ends_line_level = NULL;
+            }
+            if( $char === '"' && $prev_char != '\\' ) {
+                $in_quotes = !$in_quotes;
+            } else if( ! $in_quotes ) {
+                switch( $char ) {
+                    case '}': case ']':
+                    $level--;
+                    $ends_line_level = NULL;
+                    $new_line_level = $level;
+                    break;
+
+                    case '{': case '[':
+                    $level++;
+                    case ',':
+                        $ends_line_level = $level;
+                        break;
+
+                    case ':':
+                        $post = " ";
+                        break;
+
+                    case " ": case "\t": case "\n": case "\r":
+                    $char = "";
+                    $ends_line_level = $new_line_level;
+                    $new_line_level = NULL;
+                    break;
+                }
+            }
+            if( $new_line_level !== NULL ) {
+                $result .= "\n".str_repeat( "\t", $new_line_level );
+            }
+            $result .= $char.$post;
+            $prev_char = $char;
+        }
+
+        return $result;
     }
 }
