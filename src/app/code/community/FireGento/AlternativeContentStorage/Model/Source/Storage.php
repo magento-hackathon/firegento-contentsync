216239d786c8ce3742a403d1b3673042f95d78c4
Francesco Marangi <francesco.marangi@gmail.com>
2013-01-12 20:00:42 +0100

    Remove 'None' option from storage type source model

--- src/app/code/community/FireGento/AlternativeContentStorage/Model/Source/Storage.php
+++ src/app/code/community/FireGento/AlternativeContentStorage/Model/Source/Storage.php
@@ -3,7 +3,6 @@
 class FireGento_AlternativeContentStorage_Model_Source_Storage
 {
 
-    const TYPE_NONE = '-';
     const TYPE_FILE = 'file';
 
     /**
@@ -16,7 +15,6 @@ class FireGento_AlternativeContentStorage_Model_Source_Storage
         // TODO: Get available storages from config file
 
         return array(
-            array('value' => self::TYPE_NONE, 'label'=>Mage::helper('acs')->__('None')),
             array('value' => self::TYPE_FILE, 'label'=>Mage::helper('acs')->__('File')),
         );
     }
