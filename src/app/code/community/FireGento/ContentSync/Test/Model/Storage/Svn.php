71fee3feab916b69e2b897fe0e9f8f4c802fa1ed
Tobias Zander <tobias.zander@sitewards.com>
2013-01-13 14:56:15 +0100

    Added SVN support

---
+++ src/app/code/community/FireGento/ContentSync/Test/Model/Storage/Svn.php
@@ -0,0 +1,8 @@
+<?php
+class FireGento_ContentSync_Test_Model_Storage_Svn extends EcomDev_PHPUnit_Test_Case {
+	public function testStoreData() {
+		$model = Mage::getModel('contentsync/storage_svn');
+		$this->assertNull($model->storeData(array(1, 2), 'cms_page'));
+
+	}
+}
