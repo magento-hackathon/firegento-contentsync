7a58414c7d7ee4dab889078be3b22bbcaf91e4df
Francesco Marangi <francesco.marangi@gmail.com>
2013-01-13 11:40:44 +0100

    Add hash models, database structure, and table hash functionality

---
+++ src/app/code/community/FireGento/ContentSync/Model/Resource/Hash.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * This file is part of the FIREGENTO project.
+ * 
+ * FireGento_AlternativeContentStorage is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ * 
+ * This script is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * PHP version 5
+ * 
+ * @category  FireGento
+ * @package   FireGento_ContentSync
+ * @author    FireGento Team <team@firegento.com>
+ * @copyright 2013 FireGento Team (http://www.firegento.de). All rights served.
+ * @license   http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
+ * @version   $Id:$
+ * @since     0.1.0
+ */
+
+/**
+ * Content Sync Hash Resource Model
+ * 
+ * @category  FireGento
+ * @package   FireGento_ContentSync
+ * @author    FireGento Team <team@firegento.com>
+ */
+class FireGento_ContentSync_Model_Resource_Hash extends Mage_Core_Model_Resource_Db_Abstract
+{
+
+    protected function _construct()
+    {
+        $this->_init('contentsync/hash', 'hash_id');
+    }
+
+    /**
+     * Perform actions before object save
+     *
+     * @param Varien_Object $object
+     * @return Mage_Core_Model_Resource_Db_Abstract
+     */
+    protected function _beforeSave(Mage_Core_Model_Abstract $object)
+    {
+        $object->setUpdatedAt(Mage::getSingleton('core/date')->date());
+        return $this;
+    }
+}
